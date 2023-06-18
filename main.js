6409b11337cf32e9affaf6efa5e0d1f70377613c
7R011 <7R011@protonmail.ch>
2023-06-18 08:43:28 -0400

    lazy api

--- main.js
+++ main.js
@@ -191,7 +191,7 @@ rest
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once("ready", () => {
-	cron.schedule("15 8 * * *", () => {
+	cron.schedule("0 9 * * *", () => {
 		axios
 			.get(
 				`https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UC2bso7dShHmrlH9EzGyH-CQ&maxResults=1&order=date&type=video&key=${process.env.yt}`
